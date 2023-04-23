554e3c4963589c6b433cb047aefcfd2fd103251e
Yury Dunets <yury.dunets.dev@gmail.com>
2023-04-23 15:08:26 +0400

    add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  iife: `${getPackageName()}.iife.js`,
+};
+
+const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: format => fileName[format],
+    },
+  },
+  test: {},
   plugins: [react()],
-})
+});
